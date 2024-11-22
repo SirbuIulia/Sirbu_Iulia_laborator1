095386609cc3997e17fbe53026c3b3605f30531e
IULIA SÎRBU <sirbuiulia05@gmail.com>
2024-11-22 20:13:44 +0200

    Modificare 2

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sirbu_Iulia_Lab1.Controllers
+{
+    public class HomeController1 : Controller
+    {
+
+        public string Index()
+        {
+            return "Bun venit, utilizatorule!";
+        }
+
+        public string Salut()
+        {
+            return "Salutare! Bine ai venit pe pagina mea web!";
+        }
+
+        public string Informatii(string nume, int varsta)
+        {
+            return $"Salut, {nume}! Ai {varsta} ani.";
+        }
+
+    }
+}
